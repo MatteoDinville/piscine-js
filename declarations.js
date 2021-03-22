2c6afd4b8f5353e0d608472db41d4522474c6277
MDINVILLE <matteo.dinville@ynov.com>
2021-03-22 16:16:18 +0100

    declarations

---
+++ declarations.js
@@ -0,0 +1,16 @@
+let espaceStr = ["`", "\\", "/", '"',"'" ]
+const arr = [4, '2']
+let obj = {
+    str :"",
+    num : 8,
+    bool : false,
+    undef : undefined,
+}
+const nested = {
+    arr = [4, undefined,'2'],
+    obj = {
+        str : "",
+        num : '2',
+        bool : true,
+    },
+};
