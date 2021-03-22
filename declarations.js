81bda9424067c46ee8223f12cf506055effafef8
MDINVILLE <matteo.dinville@ynov.com>
2021-03-22 16:45:41 +0100

    declarations

--- declarations.js
+++ declarations.js
@@ -1,4 +1,4 @@
-const espaceStr = ["`", "\\", "/", '"',"'" ]
+const espaceStr = "\`\\\/\"" 
 const arr = [4, '2']
 const obj = {
     str :"",
@@ -6,9 +6,10 @@ const obj = {
     bool : false,
     undef : undefined,
 }
+// quand on est dans un objet pour dire = c'est : 
 const nested = {
-    arr = [4, undefined,'2'],
-    obj = {
+    arr : [4, undefined,'2'],
+    obj : {
         str : "",
         num : '2',
         bool : true,
