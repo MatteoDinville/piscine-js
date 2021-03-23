c6909290bd6ea4deb5d86c4a8bbe0824f92c8a72
MDINVILLE <matteo.dinville@ynov.com>
2021-03-23 15:12:02 +0100

    first element

---
+++ last-first-kiss.js
@@ -0,0 +1,15 @@
+//function that takes an array or a string and return the first element.
+function first(element) {
+    return element[0]
+}  
+
+//that takes an array or a string and return the last element.
+function last(element) {
+    return element[element.length-1]
+}
+//function that returns an array of 2 elements the last and the first element, in that order
+function kiss(element) {
+    return [element[element.length-1],element[0]]
+}
+
+console.log(kiss([1, 2, 3]))
