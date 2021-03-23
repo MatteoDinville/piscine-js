e7b0d578118dfa4f431706547482cc9899b72e2c
MDINVILLE <matteo.dinville@ynov.com>
2021-03-23 15:41:11 +0100

    concate

--- concat-str.js
+++ concat-str.js
@@ -1,8 +1,6 @@
 //function that takes 2 arguments and concatenate them
 
 function concatStr (str1, str2) {
-    str1 = 'Hello';
-    str2 = 'World';
     return String(str1 +' '+ str2)
 }
 console.log(concatStr(1, 2));
