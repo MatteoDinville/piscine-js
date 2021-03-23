0fa9cf411ebfda82173311b9e960c1abaa07663c
MDINVILLE <matteo.dinville@ynov.com>
2021-03-23 16:56:00 +0100

    min max

---
+++ min-max.js
@@ -0,0 +1,15 @@
+function max (number, number2) {
+    if (number > number2) {
+        return number
+    }else {
+       return number
+    }
+}
+
+function min (number, number2) {
+    if (number < number2) {
+        return number
+    }else {
+       return number
+    }
+}
