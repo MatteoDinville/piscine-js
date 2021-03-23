3299edc846afdceb05d01edd99d766d7bce3810c
MDINVILLE <matteo.dinville@ynov.com>
2021-03-23 14:41:36 +0100

    args

---
+++ more-or-less.js
@@ -0,0 +1,19 @@
+function more (a) {
+    return a += 1;
+}
+console.log(more(1));
+
+function less (b) {
+    return b -= 1; 
+}
+console.log(less(1));
+
+function add (a, b) {
+   return a+b;
+}
+console.log(add(2,3));
+
+function sub (a, b) {
+    return a-b;
+}
+console.log(less(2,3));
