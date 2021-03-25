8640be168ceed88195f918ed571ba69f51d7cf6b
MDINVILLE <matteo.dinville@ynov.com>
2021-03-25 16:53:23 +0100

    index

--- index-of.js
+++ index-of.js
@@ -4,21 +4,23 @@
 
 function indexOf (index, value, fromIndex = 0) {
     for (let i = fromIndex ; i < index.length; i++) {
-        if (index[i] === value) {
+        if (index[i] == value) {
             return i // i est la position de l'index
         }
     }
     return -1
 }
+console.log(indexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2))
 
 function lastIndexOf (index, value, fromIndex = index.length-1) {
-    for (let i = index.length-1; i >= fromIndex; i--) {
-        if (index[i]=== value){
+    for (let i = fromIndex; i >= 0; i--) {
+        if (index[i]== value){
             return i;
         }
     }
     return -1
 }
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2))
 
 function includes (index, value) {
     for (let i = 0; i < index.length; i++) {
