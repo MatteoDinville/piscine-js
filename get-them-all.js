8b2c2c33180e2e85a35298b74db7835915073c0b
MDINVILLE <matteo.dinville@ynov.com>
2021-03-29 16:26:06 +0200

    first test

---
+++ get-them-all.js
@@ -0,0 +1,26 @@
+export function getArchitects () {
+
+    const element = document.getElementsByTagName('a');
+    const element2 = document.getElementsByTagName('span');
+    return [[...element], [element2]]
+}
+
+export function getClassical () {
+    const class1 = document.getElementsByClassName("classical");
+    const class2 = document.querySelectorAll("a:not(.classical)");
+    return [[...class1], class2]
+}
+
+export function getActive () {
+    const active = document.querySelectorAll("a.classical.active");
+    const active2 = document.querySelectorAll("false");
+    return [[...active], [active2]]
+
+}
+
+export function getBonannoPisano () {
+    const id = document.getElementById('BonannoPisano');
+        return [...id]
+
+}
+
