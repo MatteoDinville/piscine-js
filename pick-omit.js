819f5c30bc43dde65ff8323eaab0d00f024ba924
MDINVILLE <matteo.dinville@ynov.com>
2021-04-26 09:33:07 +0200

    pick

---
+++ pick-omit.js
@@ -0,0 +1,32 @@
+function pick (obj, keys) {
+    let newObj = {}
+    if (typeof(keys) == "string") {
+        if (Object.keys(obj).includes(keys)){
+            newObj[keys] = obj[keys]
+        }
+    } else {
+        for (let key of keys) {
+            if (Object.keys(obj).includes(key)){
+                newObj[key] = obj[key]
+            }
+        }
+    }
+    return newObj
+}
+function omit (obj, keys) {
+    let newKeys = []
+    for (let key of Object.keys(obj)) {
+        if (!keys.includes(key)) {
+            newKeys.push(key)
+        } else {
+            continue
+        }
+    }
+    return pick(obj, newKeys)
+}
+
+pick({a: "s", b: "t"}, b); // Returns {b: "t"}
+pick({a: "s", b: "t", c: "u"}, [b, c]) // Returns {b: "t", c: "u"}  
+
+omit({a: "s", b: "t"}, b); // Returns {a: "s"}
+omit({a: "s", b: "t", c: "u"}, [b, c]) // Returns {a: "s"}
