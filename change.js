c908979ca44d28a3178e93c76d9cd56381ca308f
MDINVILLE <matteo.dinville@ynov.com>
2021-04-26 09:44:06 +0200

    change

--- change.js
+++ change.js
@@ -1,17 +1,8 @@
-const sourceObject = {
-    num: 42,
-    bool: true,
-    str: 'some text',
-    log: console.log,
-  }
-
 function get (firstKey) {
     return sourceObject[firstKey]    
 }
-console.log(get("str"))
+
 
 function set (secondKey, value){
    return sourceObject[secondKey] = value
 }
-console.log(set("num",2))
-console.log(sourceObject)
